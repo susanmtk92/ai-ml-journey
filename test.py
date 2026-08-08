1f46229fd6fd00f10ae2fd9a58aa9a1b4b2bc333
Susan Moothekkattu <susan.tm92@gmail.com>
2026-08-08 19:16:22 -0400

    weather planner-conditional

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-print('Hello world')
-name = 'Alice'
-print(name,type(name))
+distance_mi = ''
+is_raining = False
+has_bike = True
+has_car = False
+has_ride_share_app = True
+
+
+
+if not distance_mi:
+    print('False')
+elif distance_mi <= 1:
+    print('True')
